ebfbb8d2af9aa270a329261255adaf41ddcf816b
Alexis Graff <alexisgraff123@gmail.com>
2025-03-28 19:32:18 -0300

    feat: changes

--- src/features/symbol/template/SymbolTemplate.tsx
+++ src/features/symbol/template/SymbolTemplate.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { symbolRepository } from "@/features/shared/repositories";
 import {SymbolTable} from "@/features/symbol/molecules/SymbolTable";
 
-
 export const SymbolTemplate = async () => {
     const result = await symbolRepository.getAll();
     const symbols = result.success ? result.data : [];
