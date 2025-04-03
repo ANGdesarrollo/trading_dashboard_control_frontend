5e0994996e3922f07f7baea6c6b45dca786d5b70
Alexis Graff <alexisgraff123@gmail.com>
2025-04-02 23:24:48 -0300

    feat: add permission to http domain

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,10 @@ const nextConfig: NextConfig = {
                 protocol: "https",
                 hostname: "api.byte2byte.net", 
             },
+            {
+                protocol: "http",
+                hostname: "api.byte2byte.net",
+            },
         ],
     },
 };
